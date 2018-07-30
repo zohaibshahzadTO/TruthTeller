3c6068caa1157f78666ae6be1944b2987e076b23
Zohaib Shahzad <zohaibshahzad.to@gmail.com>
2018-07-29 23:44:33 -0400

    Major Updates
    - OAuth Intro + Passport.js + Strategy Implementation
    - Passport.js installation
    - See README

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // root file
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
-});
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
