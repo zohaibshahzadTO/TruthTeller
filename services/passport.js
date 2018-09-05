2047509d2b24df4de0ee42b9943ee1b3d646b939
Zohaib Shahzad <zohaibshahzad.to@gmail.com>
2018-09-05 15:53:02 -0400

    Passport Callbacks
    See README

--- services/passport.js
+++ services/passport.js
@@ -13,16 +13,18 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id })
-        .then((existingUser) => {
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
           if (existingUser) {
               // we already have a record with the given profile ID
+              done(null, existingUser);
           }
           else {
             // We dont have a user record with this ID, make a new record
-            new User({ googleId: profile.id }).save();
+            new User({ googleId: profile.id })
+              .save()
+              .then(user => done(null, user));
           }
-        })
+        });
     }
   )
 );
